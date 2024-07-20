465589c2b6d55f2268183b6d814ea6092e43dec2
Sourabhkm7 <sourabhhithaldia@gmail.com>
2024-07-20 22:03:44 +0530

    first commit

---
+++ ecommerce-frontend/src/pages/login.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+
+
+const Login = () => {
+    const [gender,setGender] = useState("");
+    const [date,setDate] = useState("");
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
