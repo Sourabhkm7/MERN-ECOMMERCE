47f43820f732e9e5c0ab113b188699278ffa9c3b
Sourabhkm7 <sourabhhithaldia@gmail.com>
2024-07-29 00:32:39 +0530

    backend-secondCommit

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -1,9 +1,26 @@
 import express from "express";
 
-const port = 3000;
+//importing Routes
+import userRoute from './routes/user.js'
+import { connectDB } from "./utils/features.js";
+import { errorMiddleware } from "./middlewares/error.js";
 
-const app=express();
+const port = 4000;
 
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.send("API Working with /api/v1")
+});
+
+//Using Routes
+app.use("/api/v1/user",userRoute);
+
+app.use(errorMiddleware);
 
 app.listen(port,() => {
     console.log(`Express is working on http://localhost:${port}`)
