e2a9a066e29959d1105969d71820c15274308988
Sourabhkm7 <sourabhhithaldia@gmail.com>
2024-11-28 22:27:05 +0530

    Debugging coupon

--- ecommerce-backend/src/controllers/payment.ts
+++ ecommerce-backend/src/controllers/payment.ts
@@ -1,7 +1,19 @@
 import { TryCatch } from "../middlewares/error.js";
+import { Coupon } from "../models/coupon.js";
+import ErrorHandler from "../utils/utilty-class.js";
 
 
 
 export const newCoupon = TryCatch(async(req,res,next)=>{
-    
-})
+    const {coupon, amount} = req.body;
+
+    if(!coupon || !amount) 
+        return next(new ErrorHandler("Please Enter both coupon and amount",400))
+
+    await Coupon.create({code: coupon, amount});
+
+    return res.status(201).json({
+        success: true,
+        message: `Coupon ${coupon} Created Successfully`,
+    });
+});
--- ecommerce-backend/src/routes/payment.ts
+++ ecommerce-backend/src/routes/payment.ts
@@ -8,7 +8,5 @@ const app = express.Router();
 app.post("/coupon/new", newCoupon );
 
 
-
-
 export default app;
 
